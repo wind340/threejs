2a1483d57ee5f6b5e05a44e04aa7e97034fda75f
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-10 23:43:13 +0900

    Custom Curve practice + TubeGeometry

---
+++ study3-1/03-geometry.js
@@ -0,0 +1,105 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+class App {
+  constructor() {
+    const divContainer = document.querySelector("#webgl-container");
+    this._divContainer = divContainer;
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(renderer.domElement);
+    this._renderer = renderer;
+
+    const scene = new THREE.Scene();
+    this._scene = scene;
+
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
+    this._setupControls();
+
+    window.onresize = this.resize.bind(this);
+    this.resize();
+
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  _setupControls() {
+    new OrbitControls(this._camera, this._divContainer);
+  }
+
+  _setupCamera() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 100);
+    camera.position.z = 20;
+    this._camera = camera;
+  }
+
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this._scene.add(light);
+  }
+
+  _setupModel() {
+    class CustomSinCurve extends THREE.Curve {
+      constructor(scale) {
+        super();
+        this.scale = scale;
+      }
+      getPoint(t) {
+        const tx = t * 3 - 1.5;
+        const ty = Math.sin(2 * Math.PI * t);
+        const tz = 0;
+        return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+      }
+    }
+    const path = new CustomSinCurve(4);
+    const geometry = new THREE.TubeGeometry(path, 40, 0.8, 8);
+    const fillmaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+    const cube = new THREE.Mesh(geometry, fillmaterial);
+
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
+    const line = new THREE.LineSegments(
+      new THREE.WireframeGeometry(geometry),
+      lineMaterial
+    );
+
+    const group = new THREE.Group();
+    group.add(cube);
+    group.add(line);
+
+    this._scene.add(group);
+    this._cube = group;
+  }
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
+
+    this._renderer.setSize(width, height);
+  }
+
+  render(time) {
+    this._renderer.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time) {
+    time *= 0.0005; //second unit
+
+    // this._cube.rotation.x = time;
+    // this._cube.rotation.y = time;
+  }
+}
+
+window.onload = function () {
+  new App();
+};
