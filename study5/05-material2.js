7c8abaf70bc4f1b1f7416b88041866f4f5616cfd
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-13 15:41:57 +0900

    MeshBasicMaterial property 연습

---
+++ study5/05-material2.js
@@ -0,0 +1,101 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+class App {
+  constructor() {
+    const divContainer = document.querySelector("#webgl-container");
+    this._divContainer = divContainer;
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(renderer.domElement);
+    this._renderer = renderer;
+
+    const scene = new THREE.Scene();
+    this._scene = scene;
+
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
+    this._setupControls();
+
+    window.onresize = this.resize.bind(this);
+    this.resize();
+
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  _setupControls() {
+    new OrbitControls(this._camera, this._divContainer);
+  }
+
+  _setupModel() {
+    const material = new THREE.MeshBasicMaterial({
+      visible: true,
+      transparent: false,
+      opacity: 0.5,
+      depthTest: true,
+      depthWrite: true,
+      side: THREE.BackSide,
+      alphaTest: 0.08,
+      fog: true,
+
+      color: 0xff0000,
+      wireframe: false,
+    });
+
+    const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
+    box.position.set(-1, 0, 0);
+    this._scene.add(box);
+
+    const sphere = new THREE.Mesh(
+      new THREE.SphereGeometry(0.7, 32, 32),
+      material
+    );
+    sphere.position.set(1, 0, 0);
+    this._scene.add(sphere);
+  }
+
+  _setupCamera() {
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
+    camera.position.z = 5;
+    this._camera = camera;
+  }
+
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this._scene.add(light);
+  }
+
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
+
+    this._renderer.setSize(width, height);
+  }
+
+  render(time) {
+    this._renderer.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time) {
+    time *= 0.001; //second unit
+  }
+}
+
+window.onload = function () {
+  new App();
+};
